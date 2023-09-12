b0455cc0892ed8e047aa237421e0fe5f1850b1d4
“Mesfin <mnbejiga@madisoncollege.edu>
2023-09-11 23:40:55 -0500

    Week 2 activity complited - Listed bucket and s3 website

--- week2/s3website.py
+++ week2/s3website.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import boto3.json
+import boto3,json
 
 
-s3_client = boto3.client('s3')
+s3client = boto3.client('s3')
 
 bucket_name = "cmalin1-script2-week2-f23"
 
-bucket_response = s3_client.create_bucket(Bucket=bucket_name)
+bucket_response = s3client.create_bucket(Bucket=bucket_name)
 
 bucket_policy = {
     'Version': '2012-10-17',
@@ -21,16 +21,26 @@ bucket_policy = {
 }
 bucket_policy_string = json.dumps(bucket_policy)
 
-bucket_policy_response = s3_client.put_bucket_policy (
+bucket_policy_response = s3client.put_bucket_policy(
     Bucket=bucket_name,
     Policy=bucket_policy_string
 )
 
-put_bucket_response = client.put_bucket_website(
+put_bucket_response = s3client.put_bucket_website( 
     Bucket=bucket_name,
     WebsiteConfiguration={
     'ErrorDocument': {'Key': 'error.html'},
     'IndexDocument': {'Suffix': 'index.html'},
     }
 )
+
+indexFile = open('index.html', 'rb')
+put_index_response = s3client.put_object(Body=indexFile, Bucket=bucket_name, Key='index.html', ContentType='text/html')
+indexFile.close()
+print(put_index_response)
+
+errorFile = open('error.html', 'rb')
+put_index_response = s3client.put_object(Body=errorFile, Bucket=bucket_name, Key='error.html', ContentType='text/html')
+errorFile.close()
+print(put_index_response)
 
