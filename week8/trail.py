1474d271c54e71eadf1f79886e4ddae3d00905f7
“Mesfin <mnbejiga@madisoncollege.edu>
2023-10-29 01:14:18 -0500

    Week 8 - Lambda Admin Added

---
+++ week8/trail.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import boto3,s3enforce,json
+
+
+
+def CreateTrail(trial_name, bucket_name):
+    trailclient = boto3.client('cloudtrail')
+    try:
+        response = trailclient.create_trail(S3BucketName=bucket_name,Name=trial_name)
+        return response
+    except trailclient.exceptions.TrailAlreadyExistsException as error:
+        response = trailclient.start_logging(Name=trial_name)
+        return response
+    
+def main():
+    sts_client = boto3.client("sts")
+    account_id = sts_client.get_caller_identity()["Account"]
+    
+    bucket_name = "mesfinb-cloud-trial-wk8"
+    trail_name = "mesfinb-ct-week8"
+    
+    policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AWSCloudTrailAclCheck20150319",
+                "Effect": "Allow",
+                "Principal": {"Service": "cloudtrail.amazonaws.com"},
+                "Action": "s3:GetBucketAcl",
+                "Resource": f"arn:aws:s3:::{bucket_name}"
+            },
+            {
+                "Sid": "AWSCloudTrailWrite20150319",
+                "Effect": "Allow",
+                "Principal": {"Service": "cloudtrail.amazonaws.com"},
+                "Action": "s3:PutObject",
+                "Resource": f"arn:aws:s3:::{bucket_name}/AWSLogs/{account_id}/*",
+                "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
+            }
+        ]
+    }
+
+    response = s3enforce.CreateBucket(bucket_name)
+    policy_response  = s3enforce.SetBucketPolicy(bucket_name,json.dumps(policy))
+
+    response = CreateTrail(trail_name, bucket_name)
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
